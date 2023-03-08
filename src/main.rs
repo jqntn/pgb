8a58de8bf4e0627c105ac35f078088e730372260
Julien QUENTIN <jqntn88@gmail.com>
2023-03-09 00:29:21 +0100

    init gba

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+extern "C" fn main() -> ! {
+    loop {}
 }
